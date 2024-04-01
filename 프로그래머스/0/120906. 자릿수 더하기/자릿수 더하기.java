1b5a8747de8631b19a42ed1635e59a2a3e3dd55b
StopStone <77120604+stopstone@users.noreply.github.com>
2024-04-01 19:34:08 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.15 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String str = Integer.toString(n);
+        String[] arr = str.split("");
+
+        for (String s : arr) {
+            answer += Integer.parseInt(s);
+        }
+        return answer;
+    }
+}
