5b54b43138340fa412240f471a4854732ff374d1
StopStone <77120604+stopstone@users.noreply.github.com>
2024-04-05 00:29:41 +0900

    [level 0] Title: 7의 개수, Time: 0.03 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+
+        String s = Arrays.toString(array);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '7') {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
