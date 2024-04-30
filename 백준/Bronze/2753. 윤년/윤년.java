649d026052b8e6603786920920a43ed335572f84
StopStone <77120604+stopstone@users.noreply.github.com>
2024-04-30 22:36:41 +0900

    [Bronze V] Title: 윤년, Time: 204 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+
+        int result = ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) ? 1 : 0;
+        System.out.println(result);
+    }
+}
