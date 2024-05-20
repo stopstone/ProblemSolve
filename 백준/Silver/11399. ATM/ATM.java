4279fc4639bf0e98658724e534f9d57efcf9d54a
StopStone <77120604+stopstone@users.noreply.github.com>
2024-05-20 21:06:23 +0900

    [Silver IV] Title: ATM, Time: 136 ms, Memory: 14420 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] arr;
+    static int N;
+    static int answer;
+    static int prev;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        answer = 0;
+        arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(arr);
+        for (int i = 0; i < N; i++) {
+            answer += prev + arr[i];
+            prev += arr[i];
+        }
+        System.out.print(answer);
+    }
+}
+
