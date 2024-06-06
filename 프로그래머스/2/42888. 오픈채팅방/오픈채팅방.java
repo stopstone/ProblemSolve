8a11e45af7bdada2ed8bbc9fc1b3889c5de9f834
StopStone <77120604+stopstone@users.noreply.github.com>
2024-06-06 18:35:47 +0900

    [level 2] Title: 오픈채팅방, Time: 121.54 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,30 +1,60 @@
 import java.util.*;
 
 class Solution {
+    final static String COMMAND_ENTER = "Enter";
+    final static String COMMAND_LEAVE = "Leave";
+    final static String COMMAND_CHANGE = "Change";
+    final static String ENTER_MESSAGE = "님이 들어왔습니다.";
+    final static String LEAVE_MESSAGE = "님이 나갔습니다.";
+    final static String DELIMITER = " ";
+    
+    HashMap<String, String> users;
+    ArrayList<String[]> log;
+    String[] info;
+    String[] answer;
+    
+    
     public String[] solution(String[] record) {
-        HashMap<String, String> users = new HashMap<String, String>();
-        ArrayList<String[]> log = new ArrayList<String[]>();
+        users = new HashMap<String, String>();
+        log = new ArrayList<String[]>();
 
         for(String rec : record) {
-            String[] info = rec.split(" ");
+            info = rec.split(DELIMITER);
 
-            if(info[0].equals("Enter")) {
-                users.put(info[1], info[2]);
-                log.add(new String[]{info[1], "님이 들어왔습니다."});
+            if(info[0].equals(COMMAND_ENTER)) {
+                enterToUserInfo();
             }
-            else if(info[0].equals("Leave")) {
-                log.add(new String[]{info[1], "님이 나갔습니다."});
+            else if(info[0].equals(COMMAND_LEAVE)) {
+                leaveToUserInfo();
             }
-            else if(info[0].equals("Change")) {
-                users.replace(info[1], info[2]);
+            else if(info[0].equals(COMMAND_CHANGE)) {
+                changeToUserInfo();
             }
         }
 
-        String[] answer = new String[log.size()];
-        for(int i = 0; i < log.size(); i++) {
-            String[] temp = log.get(i);
-            answer[i] = users.get(temp[0]) + temp[1];
-        }
+        generateUserLogTable();
         return answer;
+    }
+    
+    private void enterToUserInfo() {
+        users.put(info[1], info[2]);
+        log.add(new String[]{info[1], ENTER_MESSAGE});
+    }
+    
+    private void leaveToUserInfo() {
+        log.add(new String[]{info[1], LEAVE_MESSAGE});
+    }
+    
+    private void changeToUserInfo() {
+        users.replace(info[1], info[2]);
+    }
+    
+    private void generateUserLogTable() {
+        answer = new String[log.size()];
+        
+        for(int i = 0; i < log.size(); i++) {
+        String[] temp = log.get(i);
+        answer[i] = users.get(temp[0]) + temp[1];
+        }
     }
 }
