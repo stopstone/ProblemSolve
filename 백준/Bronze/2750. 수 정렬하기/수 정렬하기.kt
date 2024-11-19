d8ac8431d15d8ee8e09e2ebe6e99d00d8d4c1a2f
StopStone <77120604+stopstone@users.noreply.github.com>
2024-11-20 00:28:36 +0900

    [Bronze II] Title: 수 정렬하기, Time: 148 ms, Memory: 14712 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.kt
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.kt
@@ -2,9 +2,19 @@ fun main() {
     val size = readln().toInt()
     val list = IntArray(size) {
         readln().toInt()
-    }.toList()
+    }
+    for (i in list.indices) {
+        var temp: Int
+        for (j in 0..<i) {
+            if (list[i] < list[j]) {
+                temp = list[i]
+                list[i] = list[j]
+                list[j] = temp
+            }
+        }
+    }
 
-    list.sorted().forEach {
+    list.forEach {
         println(it)
     }
 }
